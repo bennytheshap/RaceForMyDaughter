10edc26d574f3f471e8b590936ac275a8098694d
Ben Shapiro <ben@getdown.org>
2011-07-26 18:20:04 -0500

    Initial import.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RaceForMyDaughter::Application.config.secret_token = '70d0b4a05d675e8c2dbb3fcf95d3bbeb5c1dbf15ee33486c97cbcafd8aecae8a0e09f6144b3fdfdf3ca8a566afd30833cafc887b55ff77f06e1ba3f47fab6453'
