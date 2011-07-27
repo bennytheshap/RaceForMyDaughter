7e0b55e538068d54a75b958623c27cce46e584d9
Ben Shapiro <ben@getdown.org>
2011-07-26 23:29:16 -0500

    cleanup

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,12 +1,13 @@
-//for the new respondent form
 $(function() {
+	/*****
+		For the new respondent form
+	*****/
 	$( "#gender_and_age_form" ).buttonset();
-});
-
-//for the mama said / i say forms
-$(function() {
-	$( "#preference_expressed_form" ).buttonset();
 	
+	/*****
+		For the mama said / i say forms
+	******/
+	$( "#preference_expressed_form" ).buttonset();
 	//wire up the buttons to show/hide relevant parts of the form
 	$( "#kind_of_preference_form" ).hide();	
 	$( "#relationship_preference_any_preference_expressed_true" ).click(function(){
@@ -16,5 +17,5 @@ $(function() {
 		$( "#kind_of_preference_form" ).fadeOut();
 	});
 	
-	
 });
+
